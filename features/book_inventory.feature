773ce5651127d0e246b6c0f575f4065a0b1287b3
annhoang191 <hquynhanh19@gmail.com>
2019-01-31 15:10:27 +0700

    Updates and deletes books from the inventory

--- features/book_inventory.feature
+++ features/book_inventory.feature
@@ -15,3 +15,13 @@ Feature: Book Inventory
     Scenario: Adding a new book to the inventory
       When I submit a new book to my inventory
       Then I should see the new book in my inventory
+
+    Scenario: Changing the name of a book
+      Given I have a book in my inventory
+      When I change the title of my book
+      Then I should see the book with the new title in my inventory
+
+    Scenario: Removing a book from my inventory
+      Given I have a book in my inventory
+      When I remove a book from my inventory
+      Then I should not see it listing in my inventory anymore
--- features/step_definitions/book_inventory_steps.rb
+++ features/step_definitions/book_inventory_steps.rb
@@ -26,3 +26,31 @@ Then "I should see the new book in my inventory" do
   expect(page).to have_content("War and Peace")
   expect(page).to have_content("Leo Tolstoy")
 end
+
+Given "I have a book in my inventory" do
+  FactoryBot.create(:book,
+    user: @registered_user,
+    name: "War and Peace",
+    author: "Leo Tolstoy")
+end
+
+When "I change the title of my book" do
+  visit root_path
+  click_link "Edit"
+  fill_in "book_name", with: "New title"
+  click_button "Update Book"
+end
+
+Then "I should see the book with the new title in my inventory" do
+  expect(page).to_not have_content("War and Peace")
+  expect(page).to have_content("New title")
+end
+
+When "I remove a book from my inventory" do
+  visit root_path
+  click_link "Destroy"
+end
+
+Then "I should not see it listing in my inventory anymore" do
+  expect(page).to_not have_content("War and Peace")
+end
